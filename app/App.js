c1136573185d76f3b5ba3ae228b0448c722b9cbc
raycoledai <raycole.dai@hotmail.com>
2020-09-17 23:40:04 +1000

    Convert App.css into emotion

--- app/App.js
+++ app/App.js
@@ -1,24 +1,50 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// @flow strict
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+import logo from './logo.svg';
+
+function AppHeader() {
+  return (
+    <header
+      css={{
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: `calc(10px + 2vmin)`,
+        color: 'white',
+      }}
+    >
+      <img
+        css={{
+          height: '40vmin',
+          pointerEvents: 'none',
+        }}
+        src={logo}
+        alt={'logo'}
+      />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        css={{ color: '#61dafb' }}
+        href={'https://reactjs.org'}
+        target={'_blank'}
+        rel={'noopener noreferrer'}
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={{ textAlign: 'center' }}>
+      <AppHeader />
     </div>
   );
 }
