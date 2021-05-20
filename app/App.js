801d95ba91f0979fa98d72c47fd124ee46c8e8fd
raycoledai <raycole.dai@hotmail.com>
2021-05-21 01:31:50 +1000

    Update App with routing

--- app/App.js
+++ app/App.js
@@ -1,31 +1,67 @@
 // @flow strict
+import { Layout, Menu } from 'antd';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import Crypto, { MarketIndex } from './pages/Crypto/MarketIndex';
 import Home from './pages/Home';
+
+const { Header, Content, Sider } = Layout;
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/crypto">Crypto</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/crypto">
-            <Crypto />
-          </Route>
-        </Switch>
-      </div>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/crypto">Crypto</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Switch>
+              <Route path="/crypto">
+                <Menu mode="inline" theme="dark" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1">
+                    <Link to="/crypto/market_index">Market Index</Link>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Link to="/crypto/performance">Performance</Link>
+                  </Menu.Item>
+                </Menu>
+              </Route>
+            </Switch>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              <Switch>
+                <Route path="/home">
+                  <Home />
+                </Route>
+                <Route path="/crypto">
+                  <Crypto />
+                </Route>
+                <Route path="/crypto/market_index">
+                  <MarketIndex />
+                </Route>
+              </Switch>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     </Router>
   );
 }
